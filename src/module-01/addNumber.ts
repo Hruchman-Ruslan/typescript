968d3f8010c1dc44e37cb76b64dc0ed6a6cf20c7
Ruslan <Giovingo92@gmail.com>
2023-10-19 17:18:51 +0300

    feat: add number and person

---
+++ src/module-01/addNumber.ts
@@ -0,0 +1,17 @@
+// Js
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// add("1", "1");
+
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+// Ts
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+export default add;
