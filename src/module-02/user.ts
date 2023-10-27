24750b76385138dd842a5ae93d81a658500872bf
Ruslan <Giovingo92@gmail.com>
2023-10-27 21:33:35 +0300

    feat: Types for variables and arguments

--- src/module-02/user.ts
+++ src/module-02/user.ts
@@ -151,3 +151,127 @@ export { tupleType, date };
 const tuple: [string, ...number[]] = ["hello", 42, 100, 200]; // OK
 
 export { tuple };
+
+// step-09
+
+// Enum
+
+enum Role {
+  ADMIN,
+  USER,
+}
+
+export { Role };
+
+enum UserStatus {
+  Active = "ACTIVE",
+  Inactive = "INACTIVE",
+  Banned = "BANNED",
+}
+
+const status: UserStatus = UserStatus.Active;
+
+export { status };
+
+const enum HttpCodes {
+  OK = 200,
+  BadRequest = 400,
+  Unauthorized = 401,
+}
+
+const statusCode = HttpCodes.OK;
+// const statusCode = HttpCodes.BadRequest;
+// const statusCode = HttpCodes.Unauthorized;
+
+export { statusCode };
+
+// step-10
+
+// Union Type
+
+let mixedType: string | number | boolean;
+
+mixedType = "string"; // OK
+mixedType = 10; // OK
+mixedType = true; // OK
+// mixedType = {}; // Error: Type '{}' is not assignable to type 'string | number | boolean'.
+
+export { mixedType };
+
+function combine(param1: number | string, param2: number | string) {
+  if (typeof param1 === "number" && typeof param2 === "number") {
+    return param1 + param2;
+  } else {
+    return param1.toString() + " " + param2.toString();
+  }
+}
+
+export { combine };
+
+// step-11
+
+// Intersection Type
+
+type Employee = {
+  name: string;
+  id: number;
+};
+
+type Manager = {
+  employees: Employee[];
+};
+
+type CEO = Employee & Manager;
+
+const ceo: CEO = {
+  name: "Alice",
+  id: 1,
+  employees: [
+    {
+      name: "Bob",
+      id: 2,
+    },
+  ],
+};
+
+export { ceo };
+
+// step-12
+
+// Literal Type
+
+type OneOrTwo = 1 | 2;
+let value: OneOrTwo;
+value = 1; // OK
+value = 2; // OK
+// value = 3; // Error: Type '3' is not assignable to type 'OneOrTwo'.
+
+type YesOrNo = "yes" | "no";
+let answer: YesOrNo;
+answer = "yes"; // OK
+answer = "no"; // OK
+// answer = "maybe"; // Error: Type '"maybe"' is not assignable to type 'YesOrNo'.
+
+export { value, answer };
+
+type ButtonSize = "small" | "medium" | "large";
+
+function getButtonStyle(size: ButtonSize) {
+  switch (size) {
+    case "small":
+      return { fontSize: "10px", padding: "5px" };
+    case "medium":
+      return { fontSize: "14px", padding: "10px" };
+    case "large":
+      return { fontSize: "18px", padding: "15px" };
+    default:
+      return { fontSize: "14px", padding: "10px" };
+  }
+}
+
+const myButtonStyleSmall = getButtonStyle("small"); // OK
+const myButtonStyleMedium = getButtonStyle("medium"); // OK
+const myButtonStyleLarge = getButtonStyle("large"); // OK
+// myButtonStyle = getButtonStyle("extra-large"); // Error: Argument of type '"extra-large"' is not assignable to parameter of type 'ButtonSize'.
+
+export { myButtonStyleSmall, myButtonStyleMedium, myButtonStyleLarge };
